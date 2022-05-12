0fdbd486d9c3cd7b669262f4a5c72f48dd137711
carternadain <cnadain@gmail.com>
2022-05-12 13:33:05 -0700

    update readme file

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
-# CODE QUIZ
+# CODE QUIZ APPLICATION
 
 
 # Description 
 This repository encapsulates code created to quiz you on your skills in becoming a full stack developer. 
 
 
-
-# Screenshot of application in use
-![Screen Shot of the code quiz in use](/assets/images/code-quiz.png)
-
 # Screenshot of webpage 
-![Screen Shot of the starter page](/assets/images/starter-page.png)
+![Screen Shot of the starter page](/assets/images/starter.png)
 
 
 # Webpage Link 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,171 +1,120 @@
-var startButton = document.getElementById('start-btn');
-var quizDirections = document.getElementById('directions');
-var questionElement = document.getElementById('question');
-var answerElement = document.getElementById('answer');
-var timerEl = document.getElementById('countdown');
-var rightWrong = document.getElementById('right-wrong');
-var questionIndex = 0;
-var timeLeft = 75;
 
-quizLength = quizQuestions.length
+// display all questions for the quiz 
 
-//Start Game Function
-function startCodingQuiz() {
-    startButton.classList.add('hide');
-    quizDirections.classList.add('hide');
-    chooseQuestion()
-    countdown()
+const QuizQuiestion  = [
+    {
+        question: "Commonly used data types DO NOT include:?",
+        a: "strings",
+        b: "booleans",
+        c: "alerts",
+        d: "numbers",
+        correct: "b",
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables?",
+        a: "commas",
+        b: "curly brackets",
+        c: "quotes",
+        d: "parenthesis",
+        correct: "b",
+    },
+    {  
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
 
+
+];
+
+
+// declare all const/variables 
+let currentQuiz = 0
+let scoreCard = 0
+const quiz= document.getElementById('quiz')
+const submitButton = document.getElementById('submit')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const first = document.getElementById('first')
+const second = document.getElementById('second')
+const third = document.getElementById('third')
+const forth = document.getElementById('forth')
+
+
+// this will start the quiz
+startQuiz()
+function startQuiz() {
+
+// function to display quiz questions
+
+    deselectAnswers()
+
+    const currentQuizQuiestion = QuizQuiestion [currentQuiz]
+// html elements to display data
+    questionEl.innerText = currentQuizQuiestion .question
+    first.innerText = currentQuizQuiestion .a
+    second.innerText = currentQuizQuiestion .b
+    third.innerText = currentQuizQuiestion .c
+    forth.innerText = currentQuizQuiestion .d
+}
+// function that allows you to deselect options
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
 }
 
-function chooseQuestion() {
-    questionElement.innerHTML = "";
-    answerElement.innerHTML = "";
-    for (var i = 0; i < quizLength; i++) {
-        var userQuestion = quizQuestions[questionIndex].question;
-        questionElement.innerText = userQuestion
-        var userOptions = quizQuestions[questionIndex].options
-    }
-    userOptions.forEach(function (addOption) {
-        var button = document.createElement('button');
-        button.innerText = addOption;
-        button.classList.add('answer-btn');
-        button.addEventListener('click', chooseAnswer);
-        answerElement.appendChild(button);
+// returns the answer once selected 
+function getSelected() {
+    let answer
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
     })
-};
+    return answer
+}
 
-function chooseAnswer(event) {
-    var element = event.target
-
-    if (element.matches('button')) {
-        if (element.innerText == quizQuestions[questionIndex].answer) {
-            var createMessage = document.createElement('div');
-            createMessage.textContent = 'Correct! ';
-            createMessage.setAttribute("id", "message");
-            rightWrong.appendChild(createMessage);
-        } else {
-            timeRem = timeRem - 10;
-            var createMessage = document.createElement('div');
-            createMessage.textContent = 'Incorrect! ';
-            createMessage.setAttribute("id", "message");
-            rightWrong.appendChild(createMessage);
+window.onload = function() {
+    var seconds = 60;
+    setInterval(function() {
+      document.getElementById("timer").innerHTML =  " : " + seconds;
+      seconds--;
+      if (seconds == 00) {
+        seconds = 60;
         }
-    }
-
-   questionIndex++;
-
-    if (questionIndex >= quizLength) {
-        quizOver();
-
-    } else {
-        chooseQuestion(questionIndex);
-    }
-};
-
-function quizOver() {
-    questionElement.innerHTML = "";
-    answerElement.innerHTML = "";
-
-    var finalScore = timeRem;
-    initials = window.prompt('Your time left was ' + finalScore + ' seconds. Please enter your initials to save your time. You can view all your scores at any time by clicking "View high scores" in the top left corner.')
-    if (initials === null || "") {
-        alert('You need to enter a valid response');
-        return quizOver();
-    } else {
-        var score = {
-            initials: initials,
-            score: finalScore
-        }
-        var scoreLog = localStorage.getItem('scoreLog');
-        if (scoreLog === null) {
-            scoreLog = [];
-        } else {
-            scoreLog = JSON.parse(scoreLog);
-        }
-        scoreLog.push(score);
-        var updatedScoreLog = JSON.stringify(scoreLog);
-        localStorage.setItem("scoreLog", updatedScoreLog);
-        window.location.replace("./highscores.html");
-    }
-    window.location.reload();
-};
-
-function countdown() {
-
-    var timeInterval = setInterval(function() {
-      if (timeRem > 1) {
-        timerEl.textContent = timeRem + ' seconds remaining';
-        timerRem--;
-      }
-      else if (timeRem === 1) {
-        timerEl.textContent = timeRem + ' second remaining';
-        timeRem--;
-      } else {
-        timerEl.textContent = '';
-        clearInterval(timeInterval);
-        displayMessage();
-      };
-      
     }, 1000);
   }
 
-  function displayMessage() {
-      alert ("You ran out of time! Try to answer all the questions before your time runs out.");
-      window.location.reload();
-  }
 
-  
 
-  const questions = [
-    {
-        questionText: "Commonly used data types DO NOT include:",
-        corAnswerIndex: 2,
-        answers: [
-            "strings",
-            "booleans",
-            "alerts",
-            "numbers"
-        ]
-    },
-    {
-        questionText: "The condiditon in an if/else statement is enclosed with _________.",
-        correctAnswerIndex: 2,
-        answers: [
-            "quotes",
-            "curly brackets",
-            "parenthesis",
-            "square brackets"
-        ]
-    },
-    {
-        questionText: "Arrays in JavaScript can be used to store ________.",
-        correctAnswerIndex: 3,
-        answers: [
-            "numbers and strings",
-            "other arrays",
-            "booleans",
-            "all of the above"
-        ]
-    },
-    {
-        questionText: "String values must be enclosed within _______ when being assigned to variables.",
-        correctAnswerIndex: 2,
-        answers: [
-            "commas",
-            "curly brackets",
-            "quotes",
-            "parenthesis"
-        ]
-    },
-    {
-        questionText: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        correctAnswerIndex: 3,
-        answers: [
-            "JavaScript",
-            "terminal/bash",
-            "for loops",
-            "console.log"
-        ]
+// a submit button event listener for when you click the answer
+submitButton.addEventListener('click', () => {
+    const answer = getSelected()
+    if(answer) {
+       if(answer === QuizQuiestion [currentQuiz].correct) {
+           scoreCard++
+       }
+
+       currentQuiz++
+
+       if(currentQuiz < QuizQuiestion .length) {
+           startQuiz()
+       } else {
+           quiz.innerHTML = `
+           <h2>You answered ${scoreCard}/${QuizQuiestion .length} questions correctly</h2>
+
+           <button onclick="location.reload()">Reload</button>
+           `
+       }
     }
-]
+})
